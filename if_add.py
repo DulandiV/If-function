a0c19299f9c4dfb9f01e01d7263ed4ca03ea7b56
DulandiV <138864346+DulandiV@users.noreply.github.com>
2023-07-07 15:38:05 +0530

    Add files via upload

---
+++ if_add.py
@@ -0,0 +1,13 @@
+import pytest
+
+@pytest.mark.parametrize("num, output", [(1, 12), (2, 13), (3, 14), (4, 15)])
+
+def test_addition(num, output):
+    
+   if  result = 11 + num:
+   	result == output:
+        assert True
+    else:
+        assert False
+
+    
